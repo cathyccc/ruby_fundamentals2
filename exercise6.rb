b5675efea524e0abe96857724330cab7050909f5
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2016-08-10 14:35:53 -0400

    created check item method & added list second item on list

--- exercise6.rb
+++ exercise6.rb
@@ -9,4 +9,18 @@ end
 
 grocery_list << "rice"
 
+def checkItem(grocery_list)
+puts "What item would you want to check for?"
+item=gets.chomp
+if grocery_list.include?(item)
+    puts "You need to pick up #{item}."
+  else
+    puts "You don't need to pick up #{item} today."
+  end
+end
+
 listGroceries(grocery_list)
+
+checkItem(grocery_list)
+
+grocery_list[1]
