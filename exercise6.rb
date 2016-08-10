3c2e560ebd2b7e0251a4c6e36a49d11c46990c14
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2016-08-10 15:17:29 -0400

    deleted salmon, displayed listagain, re-organized code

--- exercise6.rb
+++ exercise6.rb
@@ -1,4 +1,4 @@
-grocery_list = ["detergent","apples","bread","milk","eggs"]
+grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
 # Puts groceries in a list
 def listGroceries(array)
@@ -6,8 +6,6 @@ def listGroceries(array)
     puts "* #{item}"
   end
 end
-
-grocery_list << "rice"
 
 # Checks whether item is on your list
 def checkItem(array)
@@ -19,18 +17,31 @@ if array.include?(item)
     puts "You don't need to pick up #{item} today."
   end
 end
+# Lists groceries
+listGroceries(grocery_list)
 
-# # Lists groceries
-# listGroceries(grocery_list)
-#
-# # Counts number of items on your list
-# puts "You have #{grocery_list.count} number of items."
-#
-# # Checks for your item
-# puts checkItem(grocery_list)
-#
-# # Puts second item on your list
-# puts "The second item on your list is #{grocery_list[1]}."
+# Adds rice to end of list
+grocery_list << "rice"
 
+# Lists unsorted groceries
+listGroceries(grocery_list)
+
+# Counts number of items on your list
+puts "You have #{grocery_list.count} number of items."
+
+# Checks for your item on your list
+puts checkItem(grocery_list)
+
+# Names second item on your list
+puts "The second item on your list is #{grocery_list[1]}."
+
+# Lists sorted grocery list
+sorted_array = grocery_list.sort
+listGroceries(sorted_array)
+
+# Removing salmon
+grocery_list.delete("salmon")
+
+# Prints sorted grocery list
 sorted_array = grocery_list.sort
 listGroceries(sorted_array)
