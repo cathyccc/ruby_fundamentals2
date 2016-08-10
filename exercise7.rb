5406f45851abfd317b01ec1cf2db8ca0f3596e08
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2016-08-10 15:34:35 -0400

    created method to display cohort name and # of students

--- exercise7.rb
+++ exercise7.rb
@@ -3,3 +3,11 @@ students = {
   cohort2: 42,
   cohort3: 22
 }
+
+def numOfStudents(array)
+  array.each do |cohort,students|
+    puts "#{cohort}: #{students} students"
+  end
+end
+
+numOfStudents(students)
